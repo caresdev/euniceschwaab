64323efdd3cf9bdb7b51e54c4b64c086dd59b410
Gleici Pereira <gpcaresdev@gmail.com>
2024-04-19 00:29:33 -0500

    books section

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,21 +1,40 @@
 import {Image, Container, Col, Row, Button} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar} from '@fortawesome/free-solid-svg-icons';
 import '../style/Books.css';
 
 const Books = () => {
     return(
         <div className="books-section">
             <Container fluid>
-                <Row className="justify-content-center py-4">
+                <Row className="justify-content-center py-2">
                     <Col xs={10}>
-                        <h2 className="my-5">Schwaab&apos;s Best Selling in Amazon</h2>
+                        <h2 className="my-5 text-center">Schwaab&apos;s Best Selling in Amazon</h2>
                         <Row className='best-selling p-4'>
                             <Col>
                                 <Image src="./assets/stories-from-hell.png" alt="Stories From Hell cover" className='best-selling-cover-img'/>
                             </Col>
                             <Col>
-                                <div className='rating-stars'> Rating Stars</div>
-                                <div className='rating-comment'>Rating Comment</div>
-                                <Button></Button>
+                                <div className='rating-stars my-4'>
+                                    <Row>
+                                        <Col className='rating'>
+                                            <FontAwesomeIcon icon={faStar}/>
+                                            <FontAwesomeIcon icon={faStar}/>
+                                            <FontAwesomeIcon icon={faStar}/>
+                                            <FontAwesomeIcon icon={faStar}/>
+                                            <FontAwesomeIcon icon={faStar}/>
+                                        </Col>
+                                    </Row>
+                                </div>
+                                <div className='rating-comment my-4'>
+                                    <p> 
+                                        “A very well written book of short stories. 
+                                        This 13-year-old girl has a bright future as a horror novelist ahead of her. 
+                                        Well worth the read”.
+                                    </p>
+                                    <p>Eric Bruce</p>
+                                </div>
+                                <Button className='rating-button'>Buy Now</Button>
                             </Col>
 
                         </Row>
@@ -23,7 +42,7 @@ const Books = () => {
                 </Row>
                 <Row className="justify-content-center py-4">
                     <Col xs={10}> 
-                        <h2 className="my-5">Featured Books</h2>
+                        <h2 className="my-5 text-center">Featured Books</h2>
                         <Row className="featured-books">
                             <Col xs={3} lg={4}>
                                 <Image src="" alt="" className='book-cover'/>
