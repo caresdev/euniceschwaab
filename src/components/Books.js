912eff20d72f62009fa4b6f1b4a0279c98546575
Gleici Pereira <gpcaresdev@gmail.com>
2024-04-22 23:48:41 -0500

    added featured books

--- src/components/Books.js
+++ src/components/Books.js
@@ -5,14 +5,18 @@ import '../style/Books.css';
 
 const Books = () => {
     return(
+        
         <div className="books-section">
             <Container fluid>
+
+                {/* Best Selling */}
                 <Row className="justify-content-center py-2">
                     <Col xs={10}>
                         <h2 className="my-5 text-center">Schwaab&apos;s Best Selling in Amazon</h2>
                         <Row className='best-selling p-4'>
                             <Col>
                                 <Image src="./assets/stories-from-hell.png" alt="Stories From Hell cover" className='best-selling-cover-img'/>
+                                <h4 className='mt-3'>Stories From Hell</h4>
                             </Col>
                             <Col>
                                 <div className='rating-stars my-4'>
@@ -40,24 +44,26 @@ const Books = () => {
                         </Row>
                     </Col>
                 </Row>
+
+                {/* Featured Books */}
                 <Row className="justify-content-center py-4">
                     <Col xs={10}> 
                         <h2 className="my-5 text-center">Featured Books</h2>
                         <Row className="featured-books">
-                            <Col xs={3} lg={4}>
-                                <Image src="" alt="" className='book-cover'/>
-                                <h3 className='book-title'>Book 1</h3>
-                                <Button></Button>
+                            <Col lg={4}>
+                                <Image src="./assets/precious-poison.jpg" alt="Precious Poison cover" className='book-cover'/>
+                                <h3 className='book-title'>Precious Poison</h3>
+                                <Button className='rating-button'>Buy Now</Button>
                             </Col>
-                            <Col xs={3} lg={4}>
-                                <Image src="" alt="" className='book-cover'/>
-                                <h3 className='book-title'>Book 2</h3>
-                                <Button></Button>
+                            <Col lg={4}>
+                                <Image src="./assets/stories-from-hell.png" alt="Stories From Hell cover" className='book-cover'/>
+                                <h3 className='book-title'>Stories From Hell</h3>
+                                <Button className='rating-button'>Buy Now</Button>
                             </Col>
-                            <Col xs={3} lg={4}>
-                                <Image src="" alt="" className='book-cover'/>
-                                <h3 className='book-title'>Book 3</h3>
-                                <Button></Button>
+                            <Col lg={4}>
+                                <Image src="./assets/the-frosted-horror.jpg" alt="The Frosted Horror cover" className='book-cover'/>
+                                <h3 className='book-title'>The Frosted Horror</h3>
+                                <Button className='rating-button'>Buy Now</Button>
                             </Col>
                         </Row>
                     </Col>
