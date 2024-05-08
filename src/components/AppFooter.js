bdc8f1def16c7c24e751d9111b489c55a830ca80
Gleici Pereira <gpcaresdev@gmail.com>
2024-05-08 13:18:01 -0500

    footer media queries and layout improvement

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -11,18 +11,18 @@ const AppFooter = () => {
         <div className='app-footer'>
             <Container fluid className='justify-content-center p-3'>
 
-                <Row className='justify-content-around align-items-center my-4 p-3'>
-                    <Col className='col-sm-4 my-4'>
+                <Row className='align-items-center mx-3 my-4'>
+                    <Col className='col-md-5 my-4'>
                         <img src="./assets/logo-test.png" alt="Eunice Schwaab Logo" className="footer-profile-img py-1"/>
                     </Col>
-                    <Col className='col-sm-4'>
+                    <Col className='col-md-4 col-lg-3 col-xxl-2'>
                         <h4> Your Statement </h4>
                     </Col>
                 </Row>
 
 
-                <Row className='justify-content-around my-4 p-3'>
-                    <Col className='col-12 my-4'>
+                <Row className='mx-3 my-4'>
+                    <Col className='col-12 col-md-5 my-4'>
                         <h3 className='my-4'>Get in Touch</h3>
                         <p className='align-items-center'>
                             <a href="tel: +18328717839"><FontAwesomeIcon icon={faPhone} className="contact-icons" /> (832) 871-7839</a>
@@ -32,24 +32,23 @@ const AppFooter = () => {
                         </p>
                            
                     </Col>
-                    <Col className='col-12 my-4'>
+                    <Col className='col-6 col-md-4 col-lg-3 col-xxl-2 my-4'>
                         <h3 className="my-4"> Get Social</h3>
-                        <Row className="">
-                            <Col xs={3}>
+                        <Row className="justify-content-between">
+                            <Col xs={4}>
                                 <a href="https://www.instagram.com/euniceschwaab/"><FontAwesomeIcon icon={faInstagram} className="social-media-icons"/></a>
                             </Col>
-                            <Col xs={3}>
+                            <Col xs={4}>
                                 <a href="https://m.facebook.com/profile.php?id=100039788105973"><FontAwesomeIcon icon={faFacebook} className="social-media-icons" /></a>
                             </Col>
-                            <Col xs={3}>
+                            <Col xs={4}>
                                 <a href="https://wa.me/8328717839"><FontAwesomeIcon icon={faWhatsapp} className="social-media-icons" /></a>
                             </Col>
-
                         </Row>
                     </Col>
                 </Row>
 
-                <div className='footer-info my-4 mx-3 p-3'>
+                <div className='footer-copyrights small mb-4 mx-3 p-3'>
                     <p className="rights fw-lighter text-center"> &copy; 2024 Eunice Schwaab. All rights reserved.</p>
                 </div>
             </Container> 
