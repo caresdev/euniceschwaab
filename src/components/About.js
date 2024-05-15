0c9942c509a37e8ae2d50d1a35f52c2df08ae002
Gleici Pereira <gpcaresdev@gmail.com>
2024-05-15 10:12:10 -0500

    about image tablet modification

--- src/components/About.js
+++ src/components/About.js
@@ -15,10 +15,10 @@ const About = () => {
                         <h2 className="mt-5 mb-3 text-center fw-semibold">Meet Eunice Schwaab </h2>
                         <h3 className='mb-5 text-center'>Unveiling the Creative Mind</h3>
                         <Row className="about-container p-1 p-sm-4 align-items-center">
-                            <Col md={8} lg={6} xxl={6} className='my-3 text-center'>
+                            <Col md={10} lg={6} xxl={6} className='my-3 text-center'>
                                 <Image src="./assets/eunice-about.jpg" alt="Selfie of Eunice Schwaab" className='about-img mb-4'/>
                             </Col>
-                            <Col  md={8} lg={6} xxl={6} className='p-2 my-3'>
+                            <Col  md={10} lg={6} xxl={6} className='p-3 my-3'>
                                 <p> I am an aspiring author and artist with a passion for creating expressive works which illustrate the madness of practically
                                     everything. I like finding beauty in darkness. I like discovering how my characters will react to extreme settings and possibilities. I like
                                     everything which is creepy and weird. <br/><br/>
