a565266e1972a9a50b316611f361bebbe881b2fd
Gleici Pereira <gpcaresdev@gmail.com>
2024-07-24 19:49:24 -0500

    navbar toggler close and open behavior

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,12 +1,19 @@
 import { useState } from "react";
-import { Nav, Navbar, Container } from "react-bootstrap";
+import { Nav, Navbar, Container} from "react-bootstrap";
 import { HashLink as Link } from "react-router-hash-link";
 import '../style/NavigationBar.css';
 
 const NavigationBar = () => {
 
+  // By default menu is closed when page is loaded
   const [isOpen, setIsOpen] = useState(false);
+
+  // Set setIsOpen = true to open menu when button is toggled
   const toggleButton = () => setIsOpen(!isOpen);
+
+
+  // Handle user click and close menu 
+  const handleClick = () => setIsOpen(false);
 
 
   return (
@@ -17,16 +24,18 @@ const NavigationBar = () => {
         </Navbar.Brand>
 
         <Navbar.Toggle onClick={toggleButton} aria-controls="responsive-navbar-nav">
-          {isOpen ? 'Close Menu' : 'Open Menu'}
+          <span className={`navbar-toggler-icon ${isOpen ? 'open' : 'closed'}`}></span>
+          <span className={`closing-button ${isOpen ? 'open' : 'closed'}`}> </span>
         </Navbar.Toggle>
 
-        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end {isOpen ? 'open' : 'closed'}">
+        {/* Tie state to Navbar.Collapse */}
+        <Navbar.Collapse in={isOpen} id="responsive-navbar-nav" className={`justify-content-end ${isOpen ? 'open' : 'closed'}`}>
           <Nav className="text-center">
-            <Nav.Link as={Link} onClick={toggleButton} smooth to="/">Home</Nav.Link>
-            <Nav.Link as={Link} onClick={toggleButton} smooth to="/#books">Books</Nav.Link>
-            <Nav.Link as={Link} onClick={toggleButton} smooth to="/#arts">Gallery</Nav.Link>
-            <Nav.Link as={Link} onClick={toggleButton} smooth to="/#about">About</Nav.Link>
-            <Nav.Link as={Link} onClick={toggleButton} smooth to="/#contact">Contact</Nav.Link>
+            <Nav.Link as={Link} onClick={handleClick} smooth to="/">Home</Nav.Link>
+            <Nav.Link as={Link} onClick={handleClick} smooth to="/#books">Books</Nav.Link>
+            <Nav.Link as={Link} onClick={handleClick} smooth to="/#arts">Gallery</Nav.Link>
+            <Nav.Link as={Link} onClick={handleClick} smooth to="/#about">About</Nav.Link>
+            <Nav.Link as={Link} onClick={handleClick} smooth to="/#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
