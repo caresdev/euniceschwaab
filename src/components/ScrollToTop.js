b6ae16a0bd48404f09a9b89c59eb3debd5801dbe
Gleici Pereira <gpcaresdev@gmail.com>
2024-07-30 21:11:41 -0500

    scrollToTop button

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,40 +1,48 @@
-import { useEffect} from 'react';
-import { useState } from 'react';
+import { useEffect, useState} from 'react';
 import { useLocation } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
 
 const ScrollToTop = () => {
 
-  // Scroll to top when path changes
   const location  = useLocation();
+  const [showTop, setShowTop] = useState(false); 
 
+
+  // Scroll to Top behavior for Routing
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
 
 
-  // Show Top button when scrolling 
-  const [showTop, setShowTop] = useState(false);
 
-  const showTopButton = () => {
-    const position = document.documentElement.scrollTop;
+  // Scroll to Top Button for Scrolling
+  useEffect(() => {
 
-    if (position > 300){
-        setShowTop(true);
-    }
-    else{
-        setShowTop(false);
-    }
-  };
+    const showTopButton = () => {
+      const scrollPosition = document.documentElement.scrollTop;
 
-  window.addEventListener('scroll', showTopButton); 
+      if (scrollPosition > 300){
+          setShowTop(true);
+      }
+      else{
+          setShowTop(false);
+      }
+    };
 
-  return null;
+    const handleScrollTop = () => {
+      window.scrollTo(0);
 
-  // return (
-  //   <Button></Button>
-  // );
+    };
+
+    window.addEventListener('scroll', showTopButton); 
+
+    return (
+      <Button onClick={() => window.scrollTo(0)} className='scrollToTopBtn'> To Top </Button>
+    );
+  }, []);
+
+    
 
 };
 
